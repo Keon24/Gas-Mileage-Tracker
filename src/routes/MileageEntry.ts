46b4f3fc21038206c8055de29dcce9a189334eb7
Keon Powell <keonpowell@Keons-MacBook-Air.local>
2024-12-05 22:31:44 -0800

    successfully compared the current date and past date

--- src/routes/MileageEntry.ts
+++ src/routes/MileageEntry.ts
@@ -49,11 +49,24 @@ router.post(
        
         router.get(
             '/past-days', async ( req: Request, res: Response): Promise<void> => {
-               /* Caculate the past days */
+                try {
+                
+                /* Calculate the past days */
                const currentDate: Date = new Date()
                const sevenDaysAgo: Date = new Date (currentDate);
                sevenDaysAgo.setDate(currentDate.getDate()- 7);
-            /*Retrieve entries where date is greater or equal */
+                /* Find and compare the dates */
+                const entries = await MileageEntry.find({
+                    date: {
+                        $gte: sevenDaysAgo,
+                        $lte: currentDate,
+                    },
+                }).select('milesDriven, fuelAdded').sort({date: - 1})
+                res.status(200).json(entries)
+            } catch (error) {
+                    console.error("There was an error fetching your entries")
+                    res.status(500).json({message:"Failed to fetch mileage entries"});
+            }
                /* */
                /* */
                /* */
